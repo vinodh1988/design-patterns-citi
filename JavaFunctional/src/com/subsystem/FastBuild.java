88092c62c5ae2286ae3eab3fcfff0704097eda5c
vinodh1988 <k.c.vinodh@hotmail.com>
2021-07-12 13:43:46 +0530

    facade-commit-final

---
+++ JavaFunctional/src/com/subsystem/FastBuild.java
@@ -0,0 +1,11 @@
+package com.subsystem;
+
+public class FastBuild implements Activity{
+
+	@Override
+	public void activity(String name) {
+		// TODO Auto-generated method stub
+		System.out.println("Fast Building "+name);
+	}
+
+}

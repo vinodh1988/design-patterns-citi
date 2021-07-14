c86cf7623b98aea2157c049f064ca7bc62433ba7
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-07-14 11:00:16 +0530

    composite-pattern

---
+++ MorePatterns/src/com/composite/WholeTeam.java
@@ -0,0 +1,29 @@
+package com.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WholeTeam implements Team{
+   protected String name;
+   protected Integer teamcount;
+   private List<Team> teams=new ArrayList<Team>();
+   
+   public WholeTeam(String name,int count)
+   {
+	   this.name=name;
+	   this.teamcount=count;
+   }
+   
+   public void perform() {
+	 
+	   System.out.println(name+ " The organization with "+teamcount+ " Employees "+ "is operating");
+   	
+       for(Team x:teams) {
+    	   x.perform();
+       }
+   }
+   
+   public void addTeam(Team m) {
+	   teams.add(m);
+   }
+}
